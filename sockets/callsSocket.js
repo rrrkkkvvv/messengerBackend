f327a1fe59c0f0adc54df8987aad4531d06ce5a1
Roman <136304611+rrrkkkvvv@users.noreply.github.com>
2025-09-17 21:48:04 +0200

    Calls sygnal exchange server of sdp for RTCPeerConection

--- sockets/callsSocket.js
+++ sockets/callsSocket.js
@@ -3,17 +3,30 @@ const setupCallWebSocket = async (socket, io) => {
   socket.on("joinCallsSocket", () => {
     socket.join(`calls_${_id}`);
   });
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("callEnded");
+  // socket.on("disconnect", () => {
+  //   socket.broadcast.emit("callEnded");
+  // });
+  socket.on("iceCandidate", (data) => {
+    io.of("/calls")
+      .to(`calls_${data.to}`)
+      .emit("iceCandidate", {
+        ...data,
+      });
   });
   socket.on("callUser", (data) => {
-    io.of("/calls").to(`calls_${data.userToCall}`).emit("callUser", {
-      signal: data.signalData,
+    io.of("/calls").to(`calls_${data.to}`).emit("incomingCall", {
+      sdp: data.sdp,
       from: data.from,
     });
   });
+
   socket.on("answerCall", (data) => {
-    io.of("/calls").to(`calls_${data.to}`).emit("callAccepted", data.signal);
+    io.of("/calls")
+      .to(`calls_${data.to}`)
+      .emit("callAccepted", { sdp: data.sdp, from: data.from });
+  });
+  socket.on("endCall", (data) => {
+    io.of("/calls").to(`calls_${data.callWith}`).emit("callEnded");
   });
 };
 
